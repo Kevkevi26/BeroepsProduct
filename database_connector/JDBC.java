da1ad9244a1195bcbc94abb0fa39b46881636a34
MoW2003 <shahrukhc2003@hotmail.com>
2022-05-20 23:31:46 -0300

    added database connection

--- database_connector/JDBC.java
+++ database_connector/JDBC.java
@@ -1,5 +1,37 @@
 package database_connector;
 
+import java.sql.*;
+
 public class JDBC {
-    
+    public static void main(String[] args) {
+        Connection conn = getConnection();
+        try {
+            Statement statement = conn.createStatement();
+            ResultSet results =  statement.executeQuery("SELECT * FROM players;");
+            //System.out.println();
+            while(results.next()) {
+                String id = results.getString("id_players");
+                String username = results.getString("username");
+                String password = results.getString("password");
+                System.out.println(id + " - " + username + " - " + password);
+            }
+        } catch (SQLException e) {
+            System.out.println("Could not retrieve data from the database " + e.getMessage());
+        }
+        
+    }
+    static Connection getConnection() {
+        try {
+            String serverName = "localhost";
+            String schema = "game_test";       //database naam invullen 
+            String url = "jdbc:mysql://" + serverName +  "/" + schema;
+            String username = "user";       // username invullen
+            String password = "user";       // password invullen
+            return DriverManager.getConnection(url, username, password);
+        }
+        catch (SQLException e) {
+            System.out.println("Could not connect to the database " + e.getMessage());
+        }
+        return null;
+    }
 }
